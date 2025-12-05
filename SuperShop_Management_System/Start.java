db41865ea613663545ab6934ea09997712487dce
ashfi_raihan <144367432+ashfi-raihan@users.noreply.github.com>
2025-12-05 21:09:14 +0600

    Add files via upload

---
+++ SuperShop_Management_System/Start.java
@@ -0,0 +1,12 @@
+import GUI.*;
+
+public class Start {
+    public static void main(String []args){
+        //new SellPage();
+        //new CustomerLoginPage();
+		//new CutomerLoginPage();
+		//new Welcome();
+		new LoginPage();
+        //new HomePage();
+    }
+}
