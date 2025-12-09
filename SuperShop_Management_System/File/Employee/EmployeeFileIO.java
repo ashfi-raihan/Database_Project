666903f7ddc22bb33c1436cdd9dc0b1f2bbbdc45
ashfi_raihan <144367432+ashfi-raihan@users.noreply.github.com>
2025-12-09 22:51:58 +0600

    Add files via upload

---
+++ SuperShop_Management_System/File/Employee/EmployeeFileIO.java
@@ -0,0 +1,42 @@
+package File.EmployeeIO;
+
+import Entity.*;
+import EntityList.*;
+import GUI.*;
+
+import java.io.*;
+import java.util.*;
+
+public class EmployeeFileIO{
+	
+	public static void addEmployee(Employee em){
+		try{
+			FileWriter fw = new FileWriter(new File("./File/EmployeeIO/EmployeeInfo.txt"),true);
+			fw.write(em.getName()+";"+em.getId()+";"+ em.getAge()+";"+em.getGender()+";"+
+					em.getDesignation()+";"+em.getBranch()+";"+em.getSalary() +"\n");
+			//fw.flush();		
+			fw.close();	
+		}
+		catch(Exception e){
+			System.out.println("File Not Found");
+		}	
+	}
+	
+	public static void loadAllEmployee(EmployeeList employees){
+		try{
+			Scanner sc = new Scanner(new File("./File/EmployeeIO/EmployeeInfo.txt"));
+			while(sc.hasNextLine()){
+				String data[] = sc.nextLine().split(";");
+				
+			Employee e = new Employee(data[0],data[1],Integer.parseInt(data[2]),data[3],data[4],data[5],Integer.parseInt(data[6]));
+				
+				employees.insert(e);
+			}
+			sc.close();
+		}	
+		catch(Exception e){
+				System.out.println("File not found");
+			
+		}
+	}
+}
